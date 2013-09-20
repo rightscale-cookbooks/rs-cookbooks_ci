cae8b11bd8a86bd22142c5d4dbedd01ee7d4b91f
EfrainOlivares <efrain@rightscale.com>
2013-09-20 00:09:42 -0700

    Add apt cookbook to execute apt-get update

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,8 @@
 # limitations under the License.
 #
 
+include_recipe "apt::default"
+
 node.override['languages']['ruby']['default_version'] = node['rs-cookbooks-ci']['languages']['ruby']['default_version']
 include_recipe "ruby::default"
 
