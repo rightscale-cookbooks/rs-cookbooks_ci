064e185a5e108560d32a43a0f75e96f0bcd94cda
david-vo <david.vo@rightscale.com>
2013-10-11 11:31:02 -0700

    Added indentation for list of Jenkins plugins

--- attributes/jenkins.rb
+++ attributes/jenkins.rb
@@ -1,12 +1,12 @@
 #jenkins
 default['rs-cookbooks_ci']['jenkins']['server']['plugins'] = [
-"ssh-credentials",
-"ssh-slaves",
-"ssh-agent",
-"git-client",
-"github-api",
-"git",
-"github",
-"ansicolor"
+  "ssh-credentials",
+  "ssh-slaves",
+  "ssh-agent",
+  "git-client",
+  "github-api",
+  "git",
+  "github",
+  "ansicolor"
 ]
 default['rs-cookbooks_ci']['jenkins']['git_setup']['git_config'] = '/var/lib/jenkins/hudson.plugins.git.GitSCM.xml'
