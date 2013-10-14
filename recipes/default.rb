ec4579aa551150a9c68133a50d9197dc1962fbc1
david-vo <david.vo@rightscale.com>
2013-10-14 16:55:25 -0700

    Add build-essential attribute and recipe file, change to metadata to include build-essential

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,7 +18,7 @@
 #
 
 include_recipe 'apt'
-include_recipe 'build-essential'
+include_recipe 'rs-cookbooks_ci::build-essential'
 include_recipe 'rs-cookbooks_ci::ruby'
 include_recipe 'rs-cookbooks_ci::jenkins'
 include_recipe 'rs-cookbooks_ci::vagrant'
