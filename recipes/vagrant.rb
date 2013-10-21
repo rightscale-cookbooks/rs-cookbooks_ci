5d875315bdc2aa3a6ee1d2966bd5a79f0c203d0e
Kannan Manickam <kannan@rightscale.com>
2013-10-21 16:56:43 -0700

    Workaround for install nokogiri

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -3,6 +3,14 @@ node.override['vagrant']['checksum'] = node['rs-cookbooks_ci']['vagrant']['check
 
 include_recipe "vagrant"
 
+# TODO: Remove this workaround after upgrading to Vagrant 1.3.X
+# When running `vagrant plugin install vagrant-berkshelf` there is a problem
+# with pulling in nokogiri. It fails during the build error.
+#
+execute "gem install nokogiri --install-dir /var/lib/jenkins/.vagrant.d/gems" do
+  user 'jenkins'
+end
+
 node['rs-cookbooks_ci']['vagrant']['plugins'].each do |plug_name|
   vagrant_plugin plug_name do
     user 'jenkins'
