14e1e9fb77a69cea351ca4fc92349ccb9cdb4e9c
david-vo <david.vo@rightscale.com>
2013-10-24 16:25:50 -0700

    Fixed typo

--- attributes/build-essential.rb
+++ attributes/build-essential.rb
@@ -1,3 +1,3 @@
 # Compiletime attribute for build-essential needs to be set to true so that the BCrypt gem used by the user
-# recipe can buld
+# recipe can build
 default['rs-cookbooks_ci']['build_essential']['compiletime'] = true
